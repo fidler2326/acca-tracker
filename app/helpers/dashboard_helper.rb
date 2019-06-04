e24a65c382c570321f711688ee6d7c0660a95424
Adam <fidler2326@gmail.com>
2019-06-04 18:53:23 +0100

    Fix chart values

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,7 +1,7 @@
 module DashboardHelper
   def get_day_returns date, category
-    week_return = Acca.where("date >= ? AND category = ?", Date.today - 7.days, category).sum(&:return).to_f
-    satkes_until_now = Acca.where("date <= ? AND category = ?", date, category).sum(&:stake).to_f
-    return week_return - satkes_until_now
+    total_return = Acca.where("date <= ? AND category = ?", date, category).sum(&:return).to_f
+    total_stake = Acca.where("date <= ? AND category = ?", date, category).sum(&:stake).to_f
+    return (total_return - total_stake).round(2)
   end
 end
