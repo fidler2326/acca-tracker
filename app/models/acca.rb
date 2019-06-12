12aa5377fe68c7d6d473fe664afcdcac6723ae64
Adam <fidler2326@gmail.com>
2019-06-12 21:52:11 +0100

    Add bet types

--- app/models/acca.rb
+++ app/models/acca.rb
@@ -11,15 +11,21 @@ class Acca < ActiveRecord::Base
   CATEGORIES = Category.constants.collect {|c| Category.const_get c }
 
   module Type
-    SINGLE    = "Single"
-    DOUBLE    = "Double"
-    TREBLE    = "Treble"
-    FOUR_FOLD = "4 Fold"
-    FIVE_FOLD = "5 Fold"
-    SIX_FOLD  = "6 Fold"
-    LUCKY_31  = "Lucky 31"
-    LUCKY_15  = "Lucky 15"
-    LUCKY_63  = "Lucky 63"
+    SINGLE       = "Single"
+    DOUBLE       = "Double"
+    TREBLE       = "Treble"
+    FOUR_FOLD    = "4 Fold"
+    FIVE_FOLD    = "5 Fold"
+    SIX_FOLD     = "6 Fold"
+    SEVEN_FOLD   = "7 Fold"
+    EIGHT_FOLD   = "8 Fold"
+    NINE_FOLD    = "9 Fold"
+    TEN_FOLD     = "10 Fold"
+    ELEVEN_FOLD  = "11 Fold"
+    TWELVE_FOLD  = "12 Fold"
+    LUCKY_31     = "Lucky 31"
+    LUCKY_15     = "Lucky 15"
+    LUCKY_63     = "Lucky 63"
   end
   BET_TYPES = Type.constants.collect {|c| Type.const_get c }
 end
